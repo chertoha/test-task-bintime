61de4413e8dff49ef5c30d9d408274ea8f750230
Anton <a.chertok@akatech.com.ua>
2023-12-03 19:35:02 +0000

    Create storage.ts

---
+++ src/services/storage.ts
@@ -0,0 +1,12 @@
+export const storage = <T>(key: string) => {
+  return {
+    set: (data: T): void => {
+      localStorage.setItem(key, JSON.stringify(data));
+    },
+
+    get: (): T => {
+      const data = localStorage.getItem(key);
+      return data && JSON.parse(data);
+    },
+  };
+};
